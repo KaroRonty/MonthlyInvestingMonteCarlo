78a325555817ab9da0dbb16b579ad93f93c42c13
Karo Rönty <39708710+KaroRonty@users.noreply.github.com>
2018-06-22 23:25:19 +0300

    Update MonthlyInvestingMonteCarlo.r

--- MonthlyInvestingMonteCarlo.r
+++ MonthlyInvestingMonteCarlo.r
@@ -10,7 +10,8 @@ n_simulations <- 1000
 expected_return <- 0.087
 monthy_reinvestment <- 100
 
-# Make a data frame for the simulations
+# Make data frames y and z for the simulation results
+# x keeps the simulations temporarily and is replaced after every simulation
 x <- as.data.frame(matrix(nrow = 252 * length_years, ncol = 1))
 y <- as.data.frame(matrix(nrow = n_simulations, ncol = 1))
 z <-y
@@ -18,7 +19,7 @@ z <-y
 # Set the starting price for each simulation
 x[1,] <- price
 
-# Monte Carlo simulation of returns for 20% volatility
+# Monte Carlo simulation of returns using 20% volatility
 volatility <- 0.2
 for (i in 1:n_simulations){
 	for (j in 1:I(tradingdays * length_years - 1)){
@@ -30,7 +31,7 @@ for (i in 1:n_simulations){
 }
 y <- unname(unlist(y))
 
-# Monte Carlo simulation of returns for 30% volatility
+# Monte Carlo simulation of returns using 30% volatility
 volatility <- 0.3
 for (i in 1:n_simulations){
 	for (j in 1:I(tradingdays * length_years - 1)){
@@ -46,18 +47,16 @@ z <- cbind(y,z)
 colnames(z) <- c("Volatility 20%", "Volatility 30%")
 # To calculate mean returns
 temp <- z
-# Filter for better plotting
+# Filter simulation results for better plotting
 z[z > 50000] <- NA
 d <- melt(z)
 d$Var1 <- NULL
 colnames(d) <- c("Volatility", "Return")
 
-# Plot results
+# Plot the results
 ggplot(d, aes(x = Return)) +
     geom_histogram(aes(y = ..count.., fill = Volatility)) +
     facet_grid(Volatility~.) +
-	# Plot the average returns for each volatility
+	# Add lines for the average returns of each volatility
 	geom_vline(xintercept=mean(temp[,1]),size=2,color=rgb(248/255,117/255,108/255,0.5)) +
 	geom_vline(xintercept=mean(temp[,2]),size=2,color=rgb(1/255,191/255,196/255,0.5))
-	
-	
